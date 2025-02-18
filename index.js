aec4d406caf57601d13a58794e1448ab7770edca
Shahar Golan <sgolan20@gmail.com>
2025-02-18 11:10:33 +0200

    Update configuration

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
+// Add this near the top of your index.js
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors());
 app.use(express.json());
 
